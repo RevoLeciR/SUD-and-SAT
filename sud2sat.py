2191060e8fcd30436351c32f31fdbe2275056ec4
ehedlin <iamerichedlin@gmail.com>
2017-04-02 09:15:08 -0700

    Add files via upload

--- sud2sat.py
+++ sud2sat.py
@@ -1,73 +1,113 @@
+import sys
+import math
+
+def returnValueAt(row, column, grid):
+	return int(grid[column-1][row-1])
+
 def convertToDecimal(x,y,z):
 	return 81 * (x-1) + 9 * (y-1)+z
 	
-def eachElementHasAtLeastOneNumber():
-	for x in range(1,10):
-		for y in range(1,10):
+def eachElementHasAtLeastOneNumber(columnNumber, outputGrid):
+	for x in range(1,columnNumber+1):
+		for y in range(1,columnNumber+1):
 			list = ""
-			for z in range(1,10):
-				list += "%s " %(convertToDecimal(x,y,z))
+			occurence = False
+			for z in range(1,columnNumber+1):
+				if(returnValueAt(x,y,outputGrid) != z and occurence == False):
+					#actually dont bother printing this out in that case
+					list += "%s " %(convertToDecimal(x,y,z))
+				elif occurence == False:
+					list = "%s " %(convertToDecimal(x,y,z))
+					occurence = True
 			print list
 		
-def eachRowHasAtMostOneOfEachNumber():
-	for y in range(1,10):
-		for z in range(1,10):
-			for x in range(1,9):
-				for i in range(x+1,10):
-					print "-%s -%s" %(convertToDecimal(x,y,z), convertToDecimal(i,y,z))
+def eachRowHasAtMostOneOfEachNumber(columnNumber, outputGrid):
+	for y in range(1,columnNumber+1):
+		for z in range(1,columnNumber+1):
+			for x in range(1,columnNumber):
+				for i in range(x+1,columnNumber+1):
+					string = ""
+					if (returnValueAt(x,y,outputGrid) != z):
+						string += "-%s " %(convertToDecimal(x,y,z))
+					if (returnValueAt(i,y,outputGrid) != z):
+						string += "-%s " %(convertToDecimal(i,y,z))
+					if(string != ""):
+						print string
 			
-def eachColumnHasAtMostOneOfEachNumber():
-	for x in range(1,10):
-		for z in range(1,10):
-			for y in range(1,9):
-				for i in range(y+1,10):
-					print "-%s -%s" %(convertToDecimal(x,y,z), convertToDecimal(x,i,z))
+def eachColumnHasAtMostOneOfEachNumber(columnNumber, outputGrid):
+	for x in range(1,columnNumber+1):
+		for z in range(1,columnNumber+1):
+			for y in range(1,columnNumber):
+				for i in range(y+1,columnNumber+1):
+					string = ""
+					if (returnValueAt(x,y,outputGrid) != z):
+						string += "-%s " %(convertToDecimal(x,y,z))
+					if (returnValueAt(x,i,outputGrid) != z):
+						string += "-%s " %(convertToDecimal(x,i,z))
+					if(string != ""):
+						print string
 			
-def eachNumberAppearsAtMostOncePerGrid():
-	for z in range(1,10):
-		for i in range(0,3):
-			for j in range(0,3):
-				for x in range(1,4):
-					for y in range(1,4):
-						for k in range(y+1,4):
-							print "-%s -%s" %(convertToDecimal(3*i+x,3*j+y,z), convertToDecimal(3*i+x,3*j+k,z))
-							for l in range(1,4):
-								print "-%s -%s" %(convertToDecimal(3*i+x,3*j+y,z), convertToDecimal(3*i+k,3*j+l,z))
-			
-
-def testNumber():
-	q = 0
-	for x in range(1,10):
-		for y in range(1,10):
-			q+=1
-	for y in range(1,10):
-		for z in range(1,10):
-			for x in range(1,9):
-				for i in range(x+1,10):
-					q+=1
-	for x in range(1,10):
-		for z in range(1,10):
-			for y in range(1,9):
-				for i in range(y+1,10):
-					q+=1
-	for z in range(1,10):
-		for i in range(0,3):
-			for j in range(0,3):
-				for x in range(1,4):
-					for y in range(1,4):
-						for k in range(y+1,4):
-							q+=1
-							for l in range(1,4):
-								q+=1
-	print q	
+def eachNumberAppearsAtMostOncePerGrid(columnNumber, outputGrid):
+	for z in range(1,columnNumber+1):
+		for i in range(0,int(math.sqrt(columnNumber))):
+			for j in range(0,int(math.sqrt(columnNumber))):
+				for x in range(1,int(math.sqrt(columnNumber)+1)):
+					for y in range(1,int(math.sqrt(columnNumber)+1)):
+						for k in range(y+1,int(math.sqrt(columnNumber)+1)):
+							string = ""
+							if (returnValueAt(int(math.sqrt(columnNumber))*i+x,int(math.sqrt(columnNumber))*j+y,outputGrid) != z):
+								string += "-%s " %(convertToDecimal(int(math.sqrt(columnNumber))*i+x,int(math.sqrt(columnNumber))*j+y,z))
+							if (returnValueAt(int(math.sqrt(columnNumber))*i+x,int(math.sqrt(columnNumber))*j+k,outputGrid) != z):
+								string += "-%s " %(convertToDecimal(int(math.sqrt(columnNumber))*i+x,int(math.sqrt(columnNumber))*j+k,z))
+							if(string != ""):
+								print string
+							for l in range(1,int(math.sqrt(columnNumber))+1):
+								string = ""
+								if (returnValueAt(int(math.sqrt(columnNumber))*i+x,int(math.sqrt(columnNumber))*j+y,outputGrid) != z):
+									string += "-%s " %(convertToDecimal(int(math.sqrt(columnNumber))*i+x,int(math.sqrt(columnNumber))*j+y,z))
+								if (returnValueAt(int(math.sqrt(columnNumber))*i+k,int(math.sqrt(columnNumber))*j+l,outputGrid) != z):
+									string += "-%s " %(convertToDecimal(int(math.sqrt(columnNumber))*i+k,int(math.sqrt(columnNumber))*j+l,z))
+								if(string != ""):
+									print string
 
 
 def main():
-	#testNumber()
-	eachElementHasAtLeastOneNumber()	
-	eachRowHasAtMostOneOfEachNumber()	
-	eachColumnHasAtMostOneOfEachNumber()
-	eachNumberAppearsAtMostOncePerGrid()
+	
+	if len(sys.argv) == 1:
+		print ("No arguments")
+	else:
+		inputGrid = ""
+		try:
+			file = open(sys.argv[1])
+			inputGrid = file.read()
+			file.close()
+		except:
+			print ("Not a valid file")
+			inputGrid = sys.argv[1]
+		columnNumber = 0
+		outputGrid = []
+		for x in inputGrid.splitlines():
+			if x.startswith('Grid'):
+				if(columnNumber != 0):	#not the beginning of the file
+					print "next puzzle: "
+					eachElementHasAtLeastOneNumber(columnNumber, outputGrid)	
+					eachRowHasAtMostOneOfEachNumber(columnNumber, outputGrid)	
+					eachColumnHasAtMostOneOfEachNumber(columnNumber, outputGrid)
+					eachNumberAppearsAtMostOncePerGrid(columnNumber, outputGrid)
+					
+					
+					outputGrid = []
+					#run the input. what form to have the input in? string
+				columnNumber = 0
+			else:
+				outputGrid.append(x)
+				columnNumber += 1
+		if columnNumber != 0:
+			print "next puzzle: "
+			eachElementHasAtLeastOneNumber(columnNumber, outputGrid)	
+			eachRowHasAtMostOneOfEachNumber(columnNumber, outputGrid)	
+			eachColumnHasAtMostOneOfEachNumber(columnNumber, outputGrid)
+			eachNumberAppearsAtMostOncePerGrid(columnNumber, outputGrid)
 		
 if __name__ == "__main__":
 	main() 
